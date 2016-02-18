2c653ff98810ff8319a6cb412931490664b5c8ba
shumingsixin <shuming_sixin@163.com>
2016-02-18 14:01:38 +0800

    no message

--- protected/controllers/ApiController.php
+++ protected/controllers/ApiController.php
@@ -25,7 +25,7 @@ class ApiController extends Controller {
     }
 
     public function init() {
-        header('Access-Control-Allow-Origin:*');
+//        header('Access-Control-Allow-Origin:*');
         header('Access-Control-Allow-Origin:http://192.168.31.169');
         header('Access-Control-Allow-Origin:http://mingyizhudao.com');    // Cross-domain access.
         header('Access-Control-Allow-Origin:http://www.mingyizhudao.com');    // Cross-domain access.
